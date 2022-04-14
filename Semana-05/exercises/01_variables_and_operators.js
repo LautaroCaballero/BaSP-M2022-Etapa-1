755b616058d4c654f67be17ca6ef0e9e5d10a5c7
Lautaro Caballero <laucaballero50@gmail.com>
2022-04-14 12:35:50 -0300

    First exercise complete

---
+++ Semana-05/exercises/01_variables_and_operators.js
@@ -0,0 +1,22 @@
+console.log('Variables and operators');
+// a.Crear dos variables numéricas y utilizar el operador suma para guardar 
+// el valor de la suma de ambos números en una 3er variable.
+console.log('-Exercise 1.a: ')
+var firstNumber = 15;
+var secondNumber = 30;
+var addition = firstNumber + secondNumber;
+console.log('The sum of both variables is:', addition);
+// Crear dos variables de tipo String y concatenarlas guardando el resultado en una 3er variable.
+console.log('-Exercise 1.b: ')
+var bFirsString = 'Hi';
+var bSecondString = 'World';
+var concat = bFirsString + bSecondString;
+console.log('The concatenation of both strings is: ', concat);
+// Crear dos variables de tipo String y sumar el largo de cada variable 
+// (cantidad de letras del string) guardando el resultado de la suma en 
+// una 3er variable (utilizar length).
+console.log('-Exercise 1.c: ');
+var cFirsString = 'Radium';
+var cSecondString = 'Rocket';
+var stringLenght = cFirsString.length + cSecondString.length;
+console.log('The length of the sum of both strings is: ', stringLenght);
