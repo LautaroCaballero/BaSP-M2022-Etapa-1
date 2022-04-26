53ceb81c8e3f60d75cebecd50871ebee0580e28e
Lautaro Caballero <laucaballero50@gmail.com>
2022-04-26 18:14:24 -0300

    BaSP-W06-2022: 'create' button and modal

--- Semana-06/javascript/employee-signup.js
+++ Semana-06/javascript/employee-signup.js
@@ -20,10 +20,30 @@ var errorPostal = document.getElementById('error-postal');
 var errorPass = document.getElementById('error-pass');
 var errorContainer = document.getElementById('error-container');
 var input = document.getElementById('input-text');
+var buttonSubmit = document.getElementById('buttonSubmit');
+
+var check = [true, true, true, true, true, true, true, true, true, true, true];
+var checkBox = document.getElementById('modal');
+var modalX = document.getElementById('modalX');
+var modalTittle = document.getElementById('modal-tittle');
+var modalName = document.getElementById('name-modal')
+var modalLname = document.getElementById('lastname-modal');
+var modalDni = document.getElementById('dni-modal');
+var modalBirth = document.getElementById('birth-modal');
+var modalTel = document.getElementById('tel-modal');
+var modalAdress = document.getElementById('adress-modal');
+var modalCity = document.getElementById('city-modal');
+var modalPostal = document.getElementById('postal-modal');
+var modalEmail = document.getElementById('email-modal');
+var modalPassword = document.getElementById('password-modal');
+var modalPasswordR = document.getElementById('passwordr-modal');
+
+
 const letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 const numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 const letterNumbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 const space = " "
+
 
 var borderRed = (element) => {
     element.style.border= '1px solid #ff0000'
@@ -31,6 +51,27 @@ var borderRed = (element) => {
 
 var borderVio = (element) => {
     element.style.border= '1px solid #292a50'
+}
+
+let calculateAge = (birthValue) => {
+    const actualDate = new Date();
+    const actualYear = parseInt(actualDate.getFullYear());
+    const actualMonth = parseInt(actualDate.getMonth()) + 1;
+    const actualDay = parseInt(actualDate.getDate());
+    const birthYear = parseInt(String(birthValue).substring(0,4));
+    const birthMonth = parseInt(String(birthValue).substring(5,7));
+    const birthDay = parseInt(String(birthValue).substring(8,10));
+
+    let age = actualYear-birthYear;
+    if(actualMonth < birthMonth) {
+        age --;
+    }
+    else if (actualMonth === birthMonth) {
+        if (actualDay < birthDay) {
+            age --;
+        }
+    }
+    return age;
 }
 
 firstName.addEventListener('blur', e => {
@@ -41,12 +82,14 @@ firstName.addEventListener('blur', e => {
     if(firstName.value.length <=3) {
         errorContainer.appendChild(error);
         borderRed(firstName);
+        check[0] = false;
     }
     else {
         for(i of firstName.value) {
             if(!letters.includes(i)) {
                 errorContainer.appendChild(error);
                 borderRed(firstName);
+                check[0] = false;
             }
     }
 }
@@ -56,6 +99,7 @@ firstName.addEventListener('focus', e => {
     e.preventDefault();
     errorContainer.innerHTML = ""
     borderVio(firstName);
+    check[0] = true;
 })
 
 lastName.addEventListener('blur', e => {
@@ -66,12 +110,14 @@ lastName.addEventListener('blur', e => {
     if(lastName.value.length <=3) {
         errorContainer.appendChild(error);
         borderRed(lastName);
+        check[1] = false;
     }
     else {
         for(i of lastName.value) {
             if(!letters.includes(i)) {
                 errorContainer.appendChild(error);
                 borderRed(lastName);
+                check[1] = false;
             }
     }
 }
@@ -80,6 +126,33 @@ lastName.addEventListener('focus', e => {
     e.preventDefault();
     errorContainer.innerHTML = ""
     borderVio(lastName);
+    check[1] = true;
+})
+
+birth.addEventListener('blur', e => {
+    e.preventDefault();
+    var error = document.createElement('h2');
+    error.textContent = 'Wrong Date';
+    error.classList.add('error-submit');
+   
+    if (birth.value === "") {
+        errorContainer.appendChild(error);
+        borderRed(birth);
+        check[2] = false;
+    }
+    else {
+        if(calculateAge(birth.value) < 18) {
+            errorContainer.appendChild(error);
+            borderRed(birth);
+            check[2] = false;
+        }
+    }   
+})
+birth.addEventListener('focus', e => {
+    e.preventDefault();
+    errorContainer.innerHTML = ""
+    borderVio(birth);
+    check[2] = true;
 })
 
 dni.addEventListener('blur', e => {
@@ -90,12 +163,14 @@ dni.addEventListener('blur', e => {
     if(dni.value.length <=7) {
         errorContainer.appendChild(error);
         borderRed(dni);
+        check[3] = false;
     }
     else {
         for(i of dni.value) {
             if(!numbers.includes(i)) {
                 errorContainer.appendChild(error);
                 borderRed(dni);
+                check[3] = false;
             }
         }
     }
@@ -104,7 +179,10 @@ dni.addEventListener('focus', e => {
     e.preventDefault();
     errorContainer.innerHTML = ""
     borderVio(dni);
+    check[3] = true;
 })
+
+
 
 tel.addEventListener('blur', e => {
     e.preventDefault();
@@ -114,12 +192,14 @@ tel.addEventListener('blur', e => {
     if(tel.value.length !=10) {
         errorContainer.appendChild(error);
         borderRed(tel);
+        check[4] = false;
     }
     else {
         for(i of tel.value) {
             if(!numbers.includes(i)) {
                 errorContainer.appendChild(error);
                 borderRed(tel);
+                check[4] = false;
             }
         }
     }
@@ -128,6 +208,7 @@ tel.addEventListener('focus', e => {
     e.preventDefault();
     errorContainer.innerHTML = ""
     borderVio(tel);
+    check[4] = true;
 })
 
 adress.addEventListener('blur', e => {
@@ -138,12 +219,14 @@ adress.addEventListener('blur', e => {
     if(adress.value.length <=4) {
         errorContainer.appendChild(error);
         borderRed(adress);
+        check[5] = false;
     }
     else {
         for(i of adress.value) {
             if(!letterNumbers.includes(i) && !space.includes(i)) {
                 errorContainer.appendChild(error);
                 borderRed(adress);
+                check[5] = false;
             }
         }
     }
@@ -152,6 +235,7 @@ adress.addEventListener('focus', e => {
     e.preventDefault();
     errorContainer.innerHTML = ""
     borderVio(adress);
+    check[5] = true;
 })
 
 city.addEventListener('blur', e => {
@@ -162,12 +246,14 @@ city.addEventListener('blur', e => {
     if(city.value.length <=4) {
         errorContainer.appendChild(error);
         borderRed(city);
+        check[6] = false;
     }
     else {
         for(i of city.value) {
             if(!letterNumbers.includes(i)&& !space.includes(i)) {
                 errorContainer.appendChild(error);
                 borderRed(city);
+                check[6] = false;
             }
         }
     }
@@ -176,6 +262,7 @@ city.addEventListener('focus', e => {
     e.preventDefault();
     errorContainer.innerHTML = ""
     borderVio(city);
+    check[6] = true;
 })
 
 postalCode.addEventListener('blur', e => {
@@ -186,12 +273,14 @@ postalCode.addEventListener('blur', e => {
     if(postalCode.value.length <= 3 || postalCode.value.length >= 6) {
         errorContainer.appendChild(error);
         borderRed(postalCode);
+        check[7] = false;
     }
     else {
         for(i of postalCode.value) {
             if(!numbers.includes(i)) {
                 errorContainer.appendChild(error);
                 borderRed(postalCode);
+                check[7] = false;
             }
         }
     }
@@ -200,6 +289,7 @@ postalCode.addEventListener('focus', e => {
     e.preventDefault();
     errorContainer.innerHTML = ""
     borderVio(postalCode);
+    check[7] = true;
 })
 
 email.addEventListener('blur', e =>{
@@ -211,12 +301,14 @@ email.addEventListener('blur', e =>{
     if (!regexEmail.test(email.value)) {
         errorContainer.appendChild(error);
         borderRed(email);
+        check[8] = false;
     }
 })
 email.addEventListener('focus', e => {
     e.preventDefault();
     errorContainer.innerHTML = ""
     borderVio(email);
+    check[8] = true;
 })
 
 password.addEventListener('blur', e => {
@@ -227,20 +319,24 @@ password.addEventListener('blur', e => {
     if(password.value.length <=8) {
         errorContainer.appendChild(error);
         borderRed(password);
+        check[9] = false;
     }
     else {
         for(i of password.value) {
             if(!letterNumbers.includes(i)) {
                 errorContainer.appendChild(error);
                 borderRed(password);
+                check[9] = false;
             }
         }
     }
 })
+
 password.addEventListener('focus', e => {
     e.preventDefault();
     errorContainer.innerHTML = ""
     borderVio(password);
+    check[9] = true;
 })
 
 passwordRepeat.addEventListener('blur', e => {
@@ -251,12 +347,14 @@ passwordRepeat.addEventListener('blur', e => {
     if(passwordRepeat.value.length <=8) {
         errorContainer.appendChild(error);
         borderRed(passwordRepeat);
+        check[10] = false;
     }
     else {
         for(i of passwordRepeat.value) {
             if(!letterNumbers.includes(i)) {
                 errorContainer.appendChild(error);
                 borderRed(passwordRepeat);
+                check[10] = false;
             }
         }
     }
@@ -265,4 +363,95 @@ passwordRepeat.addEventListener('focus', e => {
     e.preventDefault();
     errorContainer.innerHTML = ""
     borderVio(passwordRepeat);
+    check[10] = true;
 })
+
+buttonSubmit.addEventListener('click', (e) => {
+    e.preventDefault();
+    if (firstName.value.length == 0){
+        borderRed(firstName);
+    } else if (dni.value.length == 0){
+        borderRed(dni);
+    } else if (birth.value.length == 0){
+        borderRed(birth);
+    } else if (tel.value.length == 0){
+        borderRed(tel);
+    } else if (adress.value.length == 0){
+        borderRed(adress);
+    } else if (city.value.length == 0){
+        borderRed(city);
+    } else if (postalCode.value.length == 0){
+        borderRed(postalCode);
+    } else if (email.value.length == 0){
+        borderRed(email);
+    } else if (password.value.length == 0){
+        borderRed(password);
+    } else if (passwordRepeat.value.length == 0){
+        borderRed(passwordRepeat);
+    } else {
+        modal.style.display = 'block'
+        if (check[0] == false){
+            modalTittle.textContent = 'Registration failed'
+            modalName.textContent = 'Wrong Name';
+        }
+        else if (check[1] == false) {
+            modalTittle.textContent = 'Registration failed'
+            modalLname.textContent = 'Wrong last name';
+        }
+        else if (check[2] == false) {
+            modalTittle.textContent = 'Registration failed'
+            modalDni.textContent = 'Wrong Dni';
+        }
+        else if (check[3] == false) {
+            modalTittle.textContent = 'Registration failed'
+            modalBirth.textContent = 'Wrong birth date';
+        }
+        else if (check[4] == false) {
+            modalTittle.textContent = 'Registration failed'
+            modalTel.textContent = 'Wrong telphone number';
+        }
+        else if (check[5] == false) {
+            modalTittle.textContent = 'Registration failed'
+            modalAdress.textContent = 'Wrong adress';
+        }
+        else if (check[6] == false) {
+            modalTittle.textContent = 'Registration failed'
+            modalCity.textContent = 'Wrong city';
+        }
+        else if (check[7] == false) {
+            modalTittle.textContent = 'Registration failed' 
+            modalPostal.textContent = 'Wrong postal code';
+        }
+        else if (check[8] == false) {
+            modalTittle.textContent = 'Registration failed'
+            modalEmail.textContent = 'Wrong email';
+        }
+        else if (check[9] == false) {
+            modalTittle.textContent = 'Registration failed'
+            modalPassword.textContent = 'Wrong password';
+        }
+        else if (check[10] == false) {
+            modalTittle.textContent = 'Registration failed'
+            modalPasswordR.textContent = 'Wrong password';
+        }
+        else {
+            modalTittle.textContent = 'Registration successful'
+            modalName.textContent = 'Valid name: ' + firstName.value
+            modalLname.textContent = 'Valid last name: ' + lastName.value
+            modalDni.textContent = 'Valid dni: ' + dni.value
+            modalBirth.textContent = 'Valid birth date: ' + birth.value
+            modalTel.textContent = 'Valid tel: ' + tel.value
+            modalAdress.textContent = 'Valid adress: ' + adress.value
+            modalCity.textContent = 'Valid city: ' + city.value
+            modalPostal.textContent = 'Valid postal code: ' + postalCode.value
+            modalEmail.textContent = 'Valid email: ' + email.value
+            modalPassword.textContent = 'Valid password: ' + password.value
+            modalPasswordR.textContent = 'Valid password: ' + passwordRepeat.value
+}
+        }
+    })
+
+    modalX.addEventListener('click', e => {
+        e.preventDefault();
+        modal.style.display = 'none';
+    })
--- Semana-06/javascript/login.js
+++ Semana-06/javascript/login.js
@@ -8,6 +8,13 @@ const password = document.getElementById('password');
 const letterNumbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 const errorDiv = document.createElement('div');
 
+var check = [true, true];
+var checkBox = document.getElementById('modal');
+var modalX = document.getElementById('modalX');
+var modalTittle = document.getElementById('modal-tittle');
+var modalEmail = document.getElementById('email-modal')
+var modalPassword = document.getElementById('password-modal');
+
 var borderRed = (element) => {
     element.style.borderBottom = '1px solid #ff0000'
 }
@@ -16,7 +23,7 @@ var borderVio = (element) => {
     element.style.borderBottom = '1px solid #292a50'
 }
 
-email.addEventListener('blur', e =>{
+email.addEventListener('blur', (e) =>{
     e.preventDefault();
     var regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
     if (!regexEmail.test(email.value)) {   
@@ -25,6 +32,7 @@ email.addEventListener('blur', e =>{
         error.classList.add('error-submit');
         errorContainer.appendChild(error);
         borderRed(email);
+        check[0] = false;
     }
 })
 email.addEventListener('focus', e => {
@@ -32,6 +40,7 @@ email.addEventListener('focus', e => {
     console.log("XD")
     errorContainer.innerHTML = ""
     borderVio(email);
+    check[0] = true;
 })
 
 password.addEventListener('blur', e => {
@@ -43,12 +52,14 @@ password.addEventListener('blur', e => {
     if(password.value.length <=8) {
         errorContainer.appendChild(error);
         borderRed(password);
+        check[1] = false;
     }
     else {
         for(i of password.value) {
             if(!letterNumbers.includes(i)) {
                 errorContainer.appendChild(error);
                 borderRed(password);
+                check[1] = false;
             }
         }
     }
@@ -57,6 +68,32 @@ password.addEventListener('focus', e => {
     e.preventDefault();
     errorContainer.innerHTML = ""
     borderVio(password);
+    check[1] = true;
 })
 
+submitBtn.addEventListener('click', e => {
+    e.preventDefault()
+    if (email.value.length == 0) {
+        borderRed(email);
+    }
+    else if (password.value.length == 0) {
+        borderRed(password);
+    }
+    else {
+        modal.style.display = 'block'
+        if (check[0] == false){
+            modalTittle.textContent = 'Registration failed'
+            modalName.textContent = 'Wrong Email';
+        }
+        else if (check[1] == false){
+            modalTittle.textContent = 'Registration failed'
+            modalName.textContent = 'Wrong password';
+        }
+    else {
+        modalTittle.textContent = 'Login successful'
+        modalEmail.textContent = 'Valid email: ' + email.value
+        modalPassword.textContent = 'Valid password: ' + password.value
+    }
+    }
+})
 
